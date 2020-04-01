7f2baf151997bb38a8b85e746521a831d41085df
Adam Swieciak <adamswiec@gmail.com>
2020-04-01 13:49:39 +0200

    Updates

---
+++ precache-manifest.8d59ad9882607017c295f627b1df952d.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e63ab7cc14470d8672792887994d6ba",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "3f16bfc2231c71a0a406",
+    "url": "/Portfolio/static/css/main.2930f4f2.chunk.css"
+  },
+  {
+    "revision": "2f3774a2d6c95412d991",
+    "url": "/Portfolio/static/js/2.29c1887d.chunk.js"
+  },
+  {
+    "revision": "2ca78df4e200ab631a9acbb7a85ee5b8",
+    "url": "/Portfolio/static/js/2.29c1887d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f16bfc2231c71a0a406",
+    "url": "/Portfolio/static/js/main.9704bd73.chunk.js"
+  },
+  {
+    "revision": "38e9353468597077cd3e",
+    "url": "/Portfolio/static/js/runtime-main.ed271e06.js"
+  },
+  {
+    "revision": "ac22292ac10282eb8f2cc30f57a7cbc7",
+    "url": "/Portfolio/static/media/Adam cert.ac22292a.jpg"
+  },
+  {
+    "revision": "654cf223f5a3a1ea05da18852aff5793",
+    "url": "/Portfolio/static/media/Adam cert1.654cf223.jpg"
+  },
+  {
+    "revision": "2ffb984d45867da43a3c3ccbe28c8bd2",
+    "url": "/Portfolio/static/media/Mainbg.2ffb984d.jpg"
+  },
+  {
+    "revision": "176da245a4fc606f73e6655cb00ffa89",
+    "url": "/Portfolio/static/media/Przechwytywanie.176da245.JPG"
+  },
+  {
+    "revision": "842eed118443745ce305ad5d0760d6e8",
+    "url": "/Portfolio/static/media/Przechwytywanie1.842eed11.JPG"
+  },
+  {
+    "revision": "e29d8e374507e8e3e590cb24d71b65fa",
+    "url": "/Portfolio/static/media/Przechwytywanie1M.e29d8e37.JPG"
+  },
+  {
+    "revision": "c8e60eb82c0fcdf69f1b747adc03c1ce",
+    "url": "/Portfolio/static/media/Przechwytywanie2.c8e60eb8.JPG"
+  },
+  {
+    "revision": "305820c6bc771fa4fbcf0253a00ac648",
+    "url": "/Portfolio/static/media/Przechwytywanie2M.305820c6.JPG"
+  },
+  {
+    "revision": "8bdeba5006a3ef635e29704d04402369",
+    "url": "/Portfolio/static/media/Przechwytywanie3.8bdeba50.JPG"
+  },
+  {
+    "revision": "8f93e132001bebe07aba4884b2bef8e8",
+    "url": "/Portfolio/static/media/Przechwytywanie3M.8f93e132.JPG"
+  },
+  {
+    "revision": "813c2ae395c0dd8f9c68abc259fea961",
+    "url": "/Portfolio/static/media/PrzechwytywanieM.813c2ae3.JPG"
+  },
+  {
+    "revision": "1184ee28d9f8c8d10feca567bfc31b87",
+    "url": "/Portfolio/static/media/UC-09YWBJTY.1184ee28.jpg"
+  },
+  {
+    "revision": "7e1b463875334107295989d78c5089a6",
+    "url": "/Portfolio/static/media/UC-8d9f6f99-6b01-4e54-a726-cc506e7e7b60.7e1b4638.jpg"
+  },
+  {
+    "revision": "c8cefc51c9a1b01d56190461e7c689db",
+    "url": "/Portfolio/static/media/UC-MZ3RJIS8.c8cefc51.jpg"
+  },
+  {
+    "revision": "a95383f6050641311b9c61f2054f6a52",
+    "url": "/Portfolio/static/media/adam.a95383f6.JPG"
+  }
+]);
